d6fb23aeeeb821591d2a3c3ae58daf8e6057f477
Robert Breunung <rbreunung@gmail.com>
2025-04-30 09:19:26 +0200

    improve logging test cases

--- src/main/java/de/antrophos/logging/LoggingDemo.java
+++ src/main/java/de/antrophos/logging/LoggingDemo.java
@@ -1,5 +1,8 @@
 package de.antrophos.logging;
 
+import static java.time.ZonedDateTime.now;
+import static java.time.format.DateTimeFormatter.ISO_OFFSET_TIME;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,8 +13,9 @@ public class LoggingDemo {
     public static void main(String[] args) {
         Logger logger = LogManager.getLogger(LoggingDemo.class);
 
-        logger.info("This is an info log message.");
+        logger.info("This is an info log message at {}.",ISO_OFFSET_TIME.format(now()));
         logger.warn("This is a warning log message.");
         logger.error("This is an error log message.");
+        logger.fatal("This is a fatal message", new RuntimeException("This is the outer Exception", new RuntimeException("This is the inner Exception")));
     }
 }
